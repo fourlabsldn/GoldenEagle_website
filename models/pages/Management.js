ea49ba336573e1e7ec2e1f0f95d77c7ea24d3c6f
lazamar <lazaronijunior@gmail.com>
2016-08-22 11:19:26 +0100

    Add management model

--- models/pages/Management.js
+++ models/pages/Management.js
@@ -17,7 +17,7 @@ const Management = new keystone.List('Management', {
 	autokey: {
 		path: 'slug',
 		from: 'title',
-		unique: true
+		unique: true,
 	},
 });
 
@@ -27,32 +27,159 @@ Management.add({
 			required: true,
 		},
 	},
-	'Landing Section', {
-		section_1: {
-			content: {
-				type: Types.Textarea,
-				height: 500,
-				label: 'Content',
+	'Landing section', {
+		landing_section: {
+			background_image: {
+				type: Types.CloudinaryImage,
+			},
+			section_1: {
+				content: {
+					type: Types.Textarea,
+					height: 200,
+					label: 'Content',
+				},
 			},
 		},
 	},
-	'Section 2', {
-		section_2: {
-			content1: {
+	'Tab 1', {
+		tab_1: {
+			title: {
+				type: Types.Text,
+				label: 'Title',
+			},
+			content_1: {
 				type: Types.Html,
 				wysiwyg: true,
-				height: 500,
+				height: 200,
 				label: 'Small paragraph',
 			},
-			content2: {
+			content_2: {
 				type: Types.Html,
 				wysiwyg: true,
-				height: 500,
+				height: 200,
 				label: 'Longer text',
 			},
 		},
-	}
-);
+  },
+  'Tab 2',
+  {
+		tab_2: {
+			title: {
+				type: Types.Text,
+				label: 'Title',
+			},
+			content_1: {
+				type: Types.Html,
+				wysiwyg: true,
+				height: 200,
+				label: 'Small paragraph',
+			},
+			content_2: {
+				type: Types.Html,
+				wysiwyg: true,
+				height: 200,
+				label: 'Longer text',
+			},
+		},
+  },
+  'Tab 3',
+  {
+		tab_3: {
+			title: {
+				type: Types.Text,
+				label: 'Title',
+			},
+			content_1: {
+				type: Types.Html,
+				wysiwyg: true,
+				height: 200,
+				label: 'Small paragraph',
+			},
+			content_2: {
+				type: Types.Html,
+				wysiwyg: true,
+				height: 200,
+				label: 'Longer text',
+			},
+		},
+  },
+  'Tab 4',
+  {
+		tab_4: {
+			title: {
+				type: Types.Text,
+				label: 'Title',
+			},
+			content_1: {
+				type: Types.Html,
+				wysiwyg: true,
+				height: 200,
+				label: 'Small paragraph',
+			},
+			content_2: {
+				type: Types.Html,
+				wysiwyg: true,
+				height: 200,
+				label: 'Longer text',
+			},
+		},
+	},
+	'Section 3', {
+		section_3: {
+			background: {
+				type: Types.CloudinaryImage,
+				label: 'Background image',
+			},
+			content_1: {
+				type: Types.Html,
+				wysiwyg: true,
+				height: 300,
+				label: 'Small paragraph',
+			},
+		},
+	},
+	'Section 4', {
+		section_4: {
+			content_1: {
+				type: Types.Html,
+				wysiwyg: true,
+				height: 200,
+				label: 'Small paragraph',
+			},
+			content_2: {
+				type: Types.Html,
+				wysiwyg: true,
+				height: 300,
+				label: 'Longer text',
+			},
+		},
+	},
+	'Section 5', {
+		section_5: {
+			content_1: {
+				type: Types.Html,
+				wysiwyg: true,
+				height: 300,
+				label: 'Small paragraph',
+			},
+		},
+	},
+	'Section 6', {
+		section_6: {
+			content_1: {
+				type: Types.Html,
+				wysiwyg: true,
+				height: 200,
+				label: 'Small paragraph',
+			},
+			content_2: {
+				type: Types.Html,
+				wysiwyg: true,
+				height: 200,
+				label: 'Longer text',
+			},
+		},
+	});
 
-Management.defaultColumns = 'section_1.content1';
+// Management.defaultColumns = 'section_1.content_1';
 Management.register();
