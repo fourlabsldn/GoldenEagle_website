ba98ed572b19f0ffb3d25116ee26c22c6ee46071
lazamar <lazaronijunior@gmail.com>
2016-08-26 10:40:54 +0100

    Add gulpfile with task changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-require('./tasks/index.js');
+
+// Import all tasks
+const tasksDir = './tasks';
+const requireFolder = require('require-dir-all');
+requireFolder(tasksDir, { recursive: true });
+
+// List all available tasks
+const gulp = require('gulp');
+const shell = require('gulp-shell');
+gulp.task('list-tasks', shell.task('gulp --tasks'));
