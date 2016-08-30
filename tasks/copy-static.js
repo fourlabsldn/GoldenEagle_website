6a0b1b10f9f0c2fbc0bc3d51d65acbf4bcf420e4
lazamar <lazaronijunior@gmail.com>
2016-08-30 09:24:49 +0100

    Move all of copy-static configuration to paths.json

--- tasks/copy-static.js
+++ tasks/copy-static.js
@@ -3,11 +3,11 @@ module.exports = taskName;
 
 const gulp = require('gulp');
 const paths = require('./paths.json');
-const path = require('path');
 
 // Config
 const origin = paths.static.src;
 const destiny = paths.static.dist;
+const folderMapping = paths.static.map;
 
 // All paths with an src
 const excludePaths = Object.keys(paths)
@@ -15,37 +15,19 @@ const excludePaths = Object.keys(paths)
 	.map(v => v.src)
 	.filter(p => p && p !== origin);
 
-// Paths relative to origin
-const folderMapping = {
-	'js_static/**/*': 'js', // eslint-disable-line quote-props
-};
-
 gulp.task(taskName, () => {
 	const mapped = Object.keys(folderMapping);
-	const include = ['**/*'];
 	const exclude = [...excludePaths, ...mapped];
-	copyFromTo(origin, destiny, include, exclude);
+	copy(origin, destiny, exclude);
 
-	// Copy mapped
-	mapped.forEach(m => {
-		const root = path.join(origin, m);
-		const dest = path.join(destiny, folderMapping[m]);
-		copyFromTo(root, dest, include, excludePaths);
-	});
+	// Copy mapped folders
+	mapped.forEach(p => copy(p, folderMapping[p], excludePaths));
 });
 
-function excludePath(p) {
-	return `!${p}`;
-}
 
-function fromOrigin(root) {
-	return p => path.join(root, p);
-}
+function copy(orig, dest, exclude) {
+	const toExclude = exclude.map(p => `!${p}`);
 
-function copyFromTo(root, dest, copy, exclude) {
-	const toCopy = copy.map(fromOrigin(root));
-	const toExclude = exclude.map(fromOrigin(root)).map(excludePath);
-	console.log('Copying:', toCopy, 'Excluding:', 'toExclude');
-	gulp.src([...toCopy, ...toExclude])
+	gulp.src([orig, ...toExclude])
 		.pipe(gulp.dest(dest));
 }
