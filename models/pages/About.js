817588157bc0001af09414a5690b25025be7654c
lazamar <lazaronijunior@gmail.com>
2016-09-01 15:41:31 +0100

    Adapt About HTML and model to new layout

--- models/pages/About.js
+++ models/pages/About.js
@@ -36,91 +36,27 @@ About.add({
   		},
   	},
 	},
-	'Tab 1', {
-		tab_1: {
-			title: {
-				type: Types.Text,
-				label: 'Title',
-			},
-			content_1: {
-				type: Types.Html,
-				wysiwyg: true,
-				height: 200,
-				label: 'Small paragraph',
-			},
-			content_2: {
-				type: Types.Html,
-				wysiwyg: true,
-				height: 200,
-				label: 'Longer text',
-			},
-		},
-  },
-  'Tab 2',
-  {
-		tab_2: {
-			title: {
-				type: Types.Text,
-				label: 'Title',
-			},
-			content_1: {
-				type: Types.Html,
-				wysiwyg: true,
-				height: 200,
-				label: 'Small paragraph',
-			},
-			content_2: {
-				type: Types.Html,
-				wysiwyg: true,
-				height: 200,
-				label: 'Longer text',
-			},
-		},
-  },
-  'Tab 3',
-  {
-		tab_3: {
-			title: {
-				type: Types.Text,
-				label: 'Title',
-			},
-			content_1: {
-				type: Types.Html,
-				wysiwyg: true,
-				height: 200,
-				label: 'Small paragraph',
-			},
-			content_2: {
-				type: Types.Html,
-				wysiwyg: true,
-				height: 200,
-				label: 'Longer text',
-			},
-		},
-  },
-  'Tab 4',
-  {
-		tab_4: {
-			title: {
-				type: Types.Text,
-				label: 'Title',
-			},
+  'Section 2', {
+    section_2: {
+      title: {
+        type: Types.Text,
+        label: 'Title',
+      },
       content_1: {
-				type: Types.Html,
-				wysiwyg: true,
-				height: 200,
-				label: 'Small paragraph',
-			},
-			content_2: {
-				type: Types.Html,
-				wysiwyg: true,
-				height: 300,
-				label: 'Longer text',
-			},
-		},
-	},
+        type: Types.Html,
+        wysiwyg: true,
+        height: 500,
+        label: 'Small paragraph',
+      },
+      content_2: {
+        type: Types.Html,
+        wysiwyg: true,
+        height: 500,
+        label: 'Longer text',
+      },
+    },
+  },
 	'Radi and Paul\'s quote', {
-		section_3: {
 			content_1: {
 				type: Types.Textarea,
 				height: 200,
