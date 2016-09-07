b167f8080acd0ae2604fad207ae37bc110cab097
lazamar <lazaronijunior@gmail.com>
2016-09-07 14:34:12 +0100

    First search API implementation

--- routes/rest/search.js
+++ routes/rest/search.js
@@ -1,24 +1,34 @@
 const keystone = require('keystone');
-
-exports = module.exports = function search(req, res) {
-  const view = new keystone.View(req, res);
-  const viewName = 'search';
-  const toTemplate = templateCreator(res, view, viewName);
-
-  getProperties()
-    .then(props => Promise.all(props.map(toTemplate)))
-    .then(templates => {
-      console.dir('Sending response:', templates);
-      res.json(templates);
-    });
-};
+const viewName = 'search';
+const { map, curry } = require('lodash/fp');
 
 function getProperties() {
   return keystone.list('Property').getAll();
 }
 
-function templateCreator(res, viewName) {
-  return function toTemplate(obj) {
-    return new Promise(resolve => res.render(viewName, obj, resolve));
-  };
+const render = curry((vName, res, content) => {
+  const defaultOptions = { layout: false };
+  const locals = Object.assign({}, defaultOptions, content);
+
+  return new Promise((resolve, reject) => {
+    res.render(vName, locals, handleResponse(resolve, reject));
+  });
+});
+
+function handleResponse(resolve, reject) {
+  return (err, data) => (err ? reject(err) : resolve(data));
 }
+
+const waitForPromises = p => Promise.all(p);
+const sendJson = curry((res, response) => res.json(response));
+const prepareResponse = properties => ({ properties });
+
+exports = module.exports = function search(req, res) {
+  const renderPropertyCard = render(viewName, res);
+  getProperties()
+  .then(map(renderPropertyCard))
+  .then(waitForPromises)
+  .then(prepareResponse)
+  .then(sendJson(res))
+  .catch((err) => res.status(500).send({ err }));
+};
