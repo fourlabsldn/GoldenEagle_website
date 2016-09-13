cf1d62c58b4de4453209b1f77ab27da2559c3a61
lazamar <lazaronijunior@gmail.com>
2016-09-13 09:49:46 +0100

    Refactor Property route

--- routes/views/property.js
+++ routes/views/property.js
@@ -9,36 +9,36 @@ const keystone = require('keystone');
  * @return {void}
  */
 exports = module.exports = (req, res, acquisitionMode) => {
-	const view = new keystone.View(req, res);
-	const locals = res.locals;
+  const view = new keystone.View(req, res);
+  const locals = res.locals;
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'property';
-	locals.acquisitionMode = acquisitionMode;
-	locals.filters = {
-		slug: req.params.slug
-	};
+  // locals.section is used to set the currently selected
+  // item in the header navigation.
+  locals.section = 'property';
+  locals.acquisitionMode = acquisitionMode;
+  locals.filters = {
+    slug: req.params.slug
+  };
 
-	// Load correct property
-	view.on('init', (next) => {
-		const propertyModel = keystone.list('Property').model;
-		const propertyQuery = propertyModel.findOne({
-			slug: locals.filters.slug
-		});
-
-		propertyQuery.exec((err, result) => {
-			if (result) {
-				locals.data = result;
-				locals.title = result.location.street1;
-				next(err);
-			} else {
-				res.render('404');
-			}
-		});
-	});
+  // Load correct property
+  view.on('init', (next) => {
+    keystone.list('Property').findWhere({
+    	slug: locals.filters.slug
+    })
+    .then(result => {
+    	if (result && result[0]) {
+        const property = result[0];
+        locals.data = property;
+        locals.title = property.location.street1;
+      	next();
+      } else {
+      	res.render('404');
+      }
+    })
+    .catch(e => console.log);
+  });
 
 	// Render the view
-	const viewName = 'property';
-	view.render(viewName);
+  const viewName = 'property';
+  view.render(viewName);
 };
