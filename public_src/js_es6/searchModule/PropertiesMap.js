449771a4b35993d9ab9dfaaf8795e95c8e6260dd
lazamar <lazaronijunior@gmail.com>
2016-09-19 16:41:08 +0100

    Add primitive info window

--- public_src/js_es6/searchModule/PropertiesMap.js
+++ public_src/js_es6/searchModule/PropertiesMap.js
@@ -1,23 +1,58 @@
 import React from 'react';
-import { Map, Marker } from './maps';
+import { Map, Marker, InfoWindow } from './maps';
 
-const PropertiesMap = props => (
-  <div style={{ width: '100%', height: '500px' }}>
-    <Map google={window.google} >
+class PropertiesMap extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      selectedPlace: '',
+      activeMarker: null,
+      showingInfoWindow: false,
+    };
 
-      {
-        props.locations.map(loc => console.log(loc) || (
-          <Marker
-            onClick={() => console.log('Clicked me')}
-            google={window.google}
-            pos={loc}
-            icon="http://res.cloudinary.com/golden-eagle/image/upload/v1473755159/g4172_lefldr.png"
-          />
-        ))
-      }
-    </Map>
-  </div>
-);
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.render = this.render.bind(this);
+  }
+
+  onMarkerClick(marker) {
+    console.log('Clicked:', marker);
+    this.setState({
+      selectedPlace: 'Anything',
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+  }
+
+  render() {
+    const props = this.props;
+    return (
+      <div style={{ width: '100%', height: '500px' }}>
+        <Map google={window.google} >
+
+          {
+            props.locations.map(loc => console.log(loc) || (
+              <Marker
+                onClick={this.onMarkerClick}
+                google={window.google}
+                pos={loc}
+                icon="http://res.cloudinary.com/golden-eagle/image/upload/v1473755159/g4172_lefldr.png"
+              />
+            ))
+          }
+
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+          >
+              <div>
+                <h1>{this.state.selectedPlace}</h1>
+              </div>
+          </InfoWindow>
+        </Map>
+      </div>
+    );
+  }
+}
 
 
 export default PropertiesMap
