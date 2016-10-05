ba56d984fe882a775827a77a10dafc61f1ed4164
lazamar <lazaronijunior@gmail.com>
2016-10-05 10:45:05 +0100

    Add Staff type

---
+++ models/collections/Staff.js
@@ -0,0 +1,21 @@
+const keystone = require('keystone');
+const Types = keystone.Field.Types;
+/**
+ * Staff Model
+ * ==========
+ */
+
+const Staff = new keystone.List('Staff');
+
+Staff.add({
+  portrait: { type: Types.CloudinaryImage },
+  name: { type: Types.Name },
+  role: { type: Types.Text },
+  info: { type: Types.Textarea, height: 150 },
+  phone: { type: Types.Text },
+  email: { type: Types.Email },
+  linkedin: { type: Types.Url },
+});
+
+Staff.defaultColumns = 'name|70%, email|20%';
+Staff.register();
