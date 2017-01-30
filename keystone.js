f2f8b04d5ef17f83f770830d4626b1798444dfb3
lazamar <lazaronijunior@gmail.com>
2017-01-30 09:56:35 +0000

    Remove posts and post-categories from admin panel

--- keystone.js
+++ keystone.js
@@ -84,7 +84,6 @@ keystone.set('email tests', require('./routes/emails'));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
-	posts: ['posts', 'post-categories'],
 	pages: [
     'abouts',
     'commercials',
